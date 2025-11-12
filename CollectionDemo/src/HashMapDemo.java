96ec396e35a2710df057eea4f397059a9353c492
Nayan Raut <nayanraut@Nayans-Laptop.local>
2025-11-12 21:08:23 +0530

    Sorted the HashMap based on Keys

---
+++ CollectionDemo/src/HashMapDemo.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(20, "Nayan");
+        map.put(40, "Rohan");
+        map.put(10, "Rohit");
+        map.put(50, "Sahil");
+        map.put(70, "Vipul");
+
+        System.out.println("Without sorting>>" +map);
+
+         //sort the hashmap based on keys
+
+        TreeMap<Integer, String> treeMap = new TreeMap<>(map);
+
+        System.out.println("After sorting>>" +treeMap);
+
+
+
+
+
+
+    }
+}
