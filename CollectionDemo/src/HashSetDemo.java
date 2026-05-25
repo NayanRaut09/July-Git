df1b59667b8617c328aaec71e6dbd578d8436916
Nayan Raut <nayanraut@Nayans-Laptop.local>
2026-05-25 21:01:20 +0530

    Adding HashSet example

---
+++ CollectionDemo/src/HashSetDemo.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+
+        HashSet hashSet = new HashSet();
+        hashSet.add("Nayan");
+        hashSet.add("Sejal");
+        hashSet.add("Rahul");
+        hashSet.add("Pawan");
+
+        System.out.println(hashSet);
+
+
+    }
+}
